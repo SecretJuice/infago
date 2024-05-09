11de6ac10b27418ec350ba8b568ee0ff90cb5987
Connor Robb <cr821@byu.edu>
2024-05-09 13:20:59 -0600

    feat: connect to mysql and basic query

--- src/main.go
+++ src/main.go
@@ -2,9 +2,25 @@ package main
 
 import (
 	"fmt"
+	infago "infago/src/infago"
 )
 
 func main() {
 
-	fmt.Printf("Hello world!")
+	mysql := infago.ConnectMySQL()
+	rows, err := mysql.Query("SELECT * FROM classes;")
+	if err != nil {
+		fmt.Errorf("ERROR: QUERY FAILED")
+		panic(err)
+	}
+
+	for rows.Next() {
+		var id int
+		var class_name string
+		var student_id int
+		if err := rows.Scan(&id, &class_name, &student_id); err != nil {
+			panic(err)
+		}
+		fmt.Printf("Class ID: %d, Class Name: %s, Student ID: %d\n", id, class_name, student_id)
+	}
 }
